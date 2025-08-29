7eea2fcf4becae53062130ef2e092d993bb702e3
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2025-08-29 11:52:55 -0700

    page 19 on pdf

---
+++ Le Chapeau Part 1/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public static NetworkManager instance;
+
+    void Awake()
+    {
+         if (instance != null && instance != this) //if an instance already exists
+                                                   //and it's not this one- destroy us
+            gameObject.SetActive(false);
+        else
+        {
+            instance = this; //set the instance
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        CreateRoom("testroom");
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Created room: " + PhotonNetwork.CurrentRoom.Name);
+    }
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();        
+    }
+
+    public void CreateRoom (string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    public void ChangeScene (string sceneName)
+    {
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+    void Update()
+    {
+        
+    }
+}
